a63d0adc0c1147737e704b20de6aa8e99b400a03
Andres <Andres@Jhary-PC>
2015-01-24 16:33:11 -0500

    proyecto

---
+++ test/test/Leer_achivoTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author Andres
+ */
+public class Leer_achivoTest {
+    
+    @Test
+    public void leerTest()
+    {
+        Leer leer = new Leer();
+        
+    }
+    
+    
+}
