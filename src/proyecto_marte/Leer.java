bdd589d3821bba32c3e20a68334ce71f07bc27c7
Andres <Andres@Jhary-PC>
2015-01-24 17:51:19 -0500

    proyecto

--- src/proyecto_marte/Leer.java
+++ src/proyecto_marte/Leer.java
@@ -5,10 +5,25 @@
  */
 package proyecto_marte;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 /**
  *
  * @author Andres
  */
 public class Leer {
+    
+    public File documento=null;
+    public FileReader fr=null;
+    public BufferedReader br = null;
+
+    public Leer(String archivo) throws FileNotFoundException {
+        this.documento = new File(archivo);
+        this.fr = new FileReader(archivo);
+        this.br = new BufferedReader(fr);
+       }
     
 }
